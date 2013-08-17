a45b30c1478a6aba0721bb3ebcb697e696ac3162
Alex Wiltschko <alex.bw@gmail.com>
2013-08-17 15:01:19 -0400

    Working on an sklearn-style API

--- spclust.py
+++ spclust.py
@@ -1,13 +1,99 @@
+# TODO
+# Try out AffinityPropagation
+# Raise errors for n_eigenvectors
+# Raise error for n_blocks
+
+
 from __future__ import division
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AffinityPropagation
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
+
+class SpectralBlockify(object):
+    """Find blocks in a matrix"""
+    def __init__(self, n_blocks=None, n_eigenvectors=None, order_within_blocks=True, ap_damping=0.5, ap_preference=None):
+        super(SpectralBlockify, self).__init__()
+        self.n_blocks = n_blocks
+        self.n_eigenvectors = n_eigenvectors
+        self.order_within_blocks = order_within_blocks
+        self.ap_damping = ap_damping
+        self.ap_preference = ap_preference
+
+    def fit(self, A):
+        """Find the blocks within A
+        """
+        # First, symmetricize and normalize A
+        this_A = self._symmetric_normalize(A)
+        # Then, get the spectrum (we'll cluster based on the matrix's eigenvectors)
+        _, vecs = self._get_spectrum(this_A)
+        # Get the 2nd to 2+n_eigenvectors eigenvectors to cluster
+        # (we ignore the first eigenvector, that's the matrix's steady-state)
+        eigvecs_to_cluster = vecs[:,-(self.n_eigenvectors+1):-1]
+        # Then, cluster the eigenvectors
+
+        if self.n_blocks != None:
+            self.block_labels_ = KMeans(self.n_blocks).fit_predict(eigvecs_to_cluster)
+        else:
+            apNode = AffinityPropagation(damping=self.ap_damping, preference=self.ap_preference).fit(A)
+            self.block_labels_ = apNode.labels_
+
+        # With the clustering, create the permutation
+        if self.order_within_blocks == False:
+            self.permutation_ = np.argsort(self.block_labels_)
+        else:
+            within_block_idx = []
+            this_permutation = []
+            
+            for i_block in np.unique(self.block_labels_):
+                # Get all of the out-of-block values
+                # For each row within the block
+                block_idx = self.block_labels_ == i_block
+                non_block_idx = self.block_labels_ != i_block
+                out_of_block_vals = this_A[np.ix_(block_idx, non_block_idx)]
+                # Find the summed probability out-of-block
+                out_of_block_probability = np.abs(out_of_block_vals).sum(1)
+                within_block_idx.append(np.argsort(out_of_block_probability))
+                # Now, create the permutation
+                this_permutation.append(np.argwhere(block_idx)[np.argsort(out_of_block_probability)].ravel())
+
+            # Now create a permutation
+            self.permutation_ = np.hstack(this_permutation)
+
+    def permute(self, A):
+        return A[np.ix_(self.permutation_, self.permutation_)]
+
+    def fit_permute(self, A):
+        self.fit(A)
+        return self.permute(A)
+
+
+    def _get_spectrum(self, A):
+        vals, vecs = np.linalg.eig(A)
+        order = np.argsort(np.abs(vals))
+        return vals[order], vecs[:,order]
+
+    def _symmetric_normalize(self, A,maxiter=500):
+        A = (A+A.T)/2.0
+        for i in xrange(maxiter):
+            prev = A.copy()
+            d = A.sum(0)
+            d[d==0] = 1.0
+            A /= d
+            d = A.sum(1)[:,None]
+            d[d==0] = 1.0
+            A /= d
+            if np.allclose(prev,A,atol=1e-10,rtol=1e-12):
+                return A
+        print 'warning: reached max iter'
+        return A
+
 
 def get_spectrum(A):
     vals, vecs = np.linalg.eig(A)
     order = np.argsort(np.abs(vals))
     return vals[order], vecs[:,order]
+
 
 def find_blockifying_perm(A,k,nclusters):
     _, vecs = get_spectrum(A)
